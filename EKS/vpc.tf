c51804e3fe03da80927a47d54ecf0c44c133a333
Rajesh <143868643+Rjesh2006@users.noreply.github.com>
2025-06-30 17:12:20 +0530

    Update vpc.tf

---
+++ EKS/vpc.tf
@@ -0,0 +1,33 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "eks-public-subnet-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "eks-private-subnet-${count.index}"
+  }
+}
+
+data "aws_availability_zones" "available" {}
